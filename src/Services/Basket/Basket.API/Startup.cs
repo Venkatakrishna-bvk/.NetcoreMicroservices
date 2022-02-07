97b5f955e84701a6610cffd03ced35a5f7bea673
venkatakrishna-bvk <venkatakrishna.bvk2@gmail.com>
2022-02-07 22:01:43 +0530

    Basket API consuming Discount gRPC with Docker

--- src/Services/Basket/Basket.API/Startup.cs
+++ src/Services/Basket/Basket.API/Startup.cs
@@ -1,4 +1,6 @@
+using Basket.API.GrpcServices;
 using Basket.API.Repositories;
+using Discount.Grpc.Protos;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -30,6 +32,12 @@ namespace Basket.API
                 options => { options.Configuration = Configuration.GetValue<string>("CacheSettings:ConnectionString"); }
                 );
             services.AddScoped<IBasketRepository, BasketRepository>();
+
+            // Grpc Configuration
+            services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>
+                (o => o.Address = new Uri(Configuration["GrpcSettings:DiscountUrl"]));
+            services.AddScoped<DiscountGrpcService>();
+
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
